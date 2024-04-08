9307ff4a113834d1957a3c83c68c594f3874a72a
elias <elias@Vostro5515>
2024-04-08 13:37:16 +0200

    Trabajo.java editado a través de la web en Bitbucket

--- src/Agenda/Principal.java
+++ src/Agenda/Principal.java
@@ -14,7 +14,7 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Hola mundo");
     }
     
 }
