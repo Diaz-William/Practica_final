fce4e9340c4a7b125d93fa22ce35ba5682b585b0
elias <elias@Vostro5515>
2024-04-22 11:48:31 +0200

    Trabajo.java editado a través de la web en Bitbucket

--- src/Agenda/Evento.java
+++ src/Agenda/Evento.java
@@ -1,11 +1,19 @@
 package Agenda;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public abstract class Evento 
 {
     //--------------------------------------------------------------------------
     //--------------------------------------------------------------------------
     //ATRIBUTOS
-
+    private int id;
+    private String nombre;
+    private String explicacion;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private boolean todoElDia;
 
     //--------------------------------------------------------------------------
     //CONSTRUCTOR
