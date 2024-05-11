b45b034545f47e2bf5d527dddf9337658b92e6ac
elias <elias@Vostro5515>
2024-05-11 17:27:35 +0200

    Trabajo.java editado a través de la web en Bitbucket

--- src/Agenda/Menu.java
+++ src/Agenda/Menu.java
@@ -206,7 +206,7 @@ public class Menu
     {
         switch (respuesta)
         {
-            case 1,2,4,5,6,7,8,9,10,11,12:
+            case 1,2,3,4,5,6,7,8,9,10,11,12:
                 Calendario.mostrarCalendario(respuesta, principal.getAnio());
                 Entrada.esperarEnter();
                 break;
